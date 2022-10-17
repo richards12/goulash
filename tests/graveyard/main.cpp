9d53fa8022688e80f3e5a55e3ded411f2b62c119
Constantinos Makrides <makrides@hpe.com>
2022-10-17 11:17:22 -0700

    Added switches in CMake and main.c to enable/disable libraries

--- tests/graveyard/main.cpp
+++ tests/graveyard/main.cpp
@@ -1,10 +1,15 @@
 #include <vector>
 #include <string>
+#include <iostream>
 #include "assert.h"
 #include "hip/hip_runtime.h"
 
-#include "suskevi.h"
+#ifdef ENABLE_FLIB
 #include "foo_mod.h"
+#endif
+
+#ifdef ENABLE_HLIB
+#include "suskevi.h"
 
 __global__
 void launchKernel(double *arr,const int size)
@@ -13,19 +18,28 @@ void launchKernel(double *arr,const int size)
   if(i < size)
     libh_add(arr[i]);
 }
+#endif
 
 int main(int argc, char** argv)
 {
    double coef1 = std::stod(argv[1]);
    double coef2 = std::stod(argv[2]);
+   double referenceRes = 1.0;
+
 
    int n = 1000;
    std::vector<double> aa(n, 1.0);
    
 
+
+#ifdef ENABLE_FLIB
+   referenceRes *= coef1;
    libf_setCoef(coef1);
    libf_mult(&(aa[0]), (int)aa.size());
+#endif // ENABLE_FLIB
 
+#ifdef ENABLE_HLIB
+   referenceRes += coef2;
    double *d_aa=NULL;
    libh_initialize(&d_aa,n,coef2);
 
@@ -43,11 +57,19 @@ int main(int argc, char** argv)
    hipDeviceSynchronize();
    hipMemcpy(&(aa[0]),d_aa,n*sizeof(double), hipMemcpyDeviceToHost);
    hipDeviceSynchronize();
+#endif // ENABLE_HLIB
    
 
 // Verification 
    for (int ii=0; ii<aa.size(); ++ii)
-    assert(aa[ii] == coef1+coef2);
+   {   
+    // assert(aa[ii] == referenceRes);
+    if(aa[ii]!=referenceRes)
+    {
+      std::cout<<"Verification Erro: aa[ii]!=referenceRes | aa[ii]="<<aa[ii]<<" referenceRes="<<referenceRes<<std::endl;
+      assert(aa[ii] == referenceRes);
+    }
+   }
 
    printf("Verification Passed!\n");
    fflush(stdout);
